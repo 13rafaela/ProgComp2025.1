b0e173b982f80430ccdb59057081969875174c16
Rafaela dos Anjos <163443768+13rafaela@users.noreply.github.com>
2025-05-22 15:27:55 -0300

    Update q1.py

--- AvaliativaComandoCondicional/q1.py
+++ AvaliativaComandoCondicional/q1.py
@@ -1,1 +1,10 @@
-
+numero = int(input('Digite um número de o á 1000:'))
+if numero > 1000:
+    print('numero inválido!')
+else:
+    mi = numero // 1000
+    cen = (numero % 1000) // 100
+    dez = (numero % 100) // 10
+    uni = numero % 10
+    soma = mi + cen + dez + uni
+    print ( 'A soma dos algarismos é:' , soma)
