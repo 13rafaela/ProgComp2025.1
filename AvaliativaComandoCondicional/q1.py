2b45d5d631b77b0bc3d50499d49c3fd974e16c23
Rafaela dos Anjos <163443768+13rafaela@users.noreply.github.com>
2025-05-23 18:16:17 -0300

    Update q1.py

--- AvaliativaComandoCondicional/q1.py
+++ AvaliativaComandoCondicional/q1.py
@@ -1,15 +1,26 @@
-num = int(input('Digite um número de 0 á 1000: '))
-while num > 1000:
-    print("Numero inválido!")
-    num = int(input('Digite um número de 0 á 1000: '))
-	
-soma = 0
-algarismo = num
-while algarismo > 0:
-    digito = algarismo % 10 
-    soma += digito 
-    algarismo = algarismo // 10 
-print('A soma dos algarismos é igual a: ', soma)
+num = int(input("Digite um número (0 a 9999): "))
+if num < 0 or num > 9999:
+    print("Número inválido! Digite um valor entre 0 e 9999.")
+else:
+    if num < 10:
+        soma = num
+    if num < 100:
+        dez = num // 10
+        uni = num % 10
+        soma = dez + uni
+if num < 1000:
+        cen = num // 100
+        dez = (num % 100) // 10
+        uni = num % 10
+        soma = cen + dez + uni
+else:
+        mi = num // 1000
+        cen = (num % 1000) // 100
+        dez = (num % 100) // 10
+        uni = num % 10
+        soma = mi + cen + dez + uni
+    
+print("A soma dos algarismos é:", soma)
 
 
 
