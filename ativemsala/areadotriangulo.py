8cd84ca2b12462c639be55fdecf0dd0efe737d3f
Rafaela dos Anjos <163443768+13rafaela@users.noreply.github.com>
2025-05-23 19:10:25 -0300

    Create areadotriangulo.py

---
+++ ativemsala/areadotriangulo.py
@@ -0,0 +1,5 @@
+print('esse programa calcula a area de um triângulo:')
+base = int(input('Digite a base do triângulo:'))
+altura = int(input('Digite a altura do triângulo:'))
+resultado = (base * altura) // 2
+print(' a área do triângulo é:', resultado)
