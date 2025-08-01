7dd9036b2cacab87f08d0506da988ffb0d43dd0a
Rafaela dos Anjos <163443768+13rafaela@users.noreply.github.com>
2025-08-01 18:29:00 -0300

    Create termo1.py

---
+++ ativemsala/termo1.py
@@ -0,0 +1,49 @@
+import random
+palavras = [
+    "ADAGA", "ADUBO", "AMIGO", "ANEXO", "ARAME", "ARARA", "ARROZ",
+    "ASILO", "ASTRO", "BAILE", "BAIXA", "BALAO", "BALSA", "BARCO",
+    "BARRO", "BEIJO", "BICHO", "BORDA", "BORRA", "BRAVO", "BREJO",
+    "BURRO", "CAIXA", "CALDO", "CANJA", "CARRO", "CARTA", "CERVO",
+    "CESTA", "CLIMA", "COBRA", "COLAR", "COQUE", "COURO", "CRAVO",
+    "DARDO", "FAIXA", "FARDO", "FENDA", "FERRO", "FESTA", "FLUOR",
+    "FORCA", "FORNO", "FORTE", "FUNDO", "GAITA", "GARRA", "GENIO",
+    "GESSO", "GRADE", "GRANA", "GRAMA", "GURIA", "GREVE", "GRUTA",
+    "HEROI", "HOTEL", "ICONE", "IMPAR", "IMUNE", "INDIO", "JUNTA",
+    "LAPIS", "LARVA", "LAZER", "LENTO", "LESTE", "LIMPO", "LIVRO",
+    "MACIO", "MAGRO", "MALHA", "MANSO", "MARCO", "METAL", "MORTE",
+    "MORRO", "MURAL", "MOVEL", "NACAO", "NINHO", "NOBRE", "NORMA",
+    "NORTE", "NUVEM", "PACTO", "PALHA", "PARDO", "PARTE", "PEDRA",
+    "PEDAL", "PEIXE", "PRADO", "PISTA", "POMBO", "POETA", "PONTO",
+    "PRATO", "PRECO", "PRESO", "PROSA", "PRUMO", "PULGA", "PULSO",
+    "QUEPE", "RAIVA", "RISCO", "RITMO", "ROSTO", "ROUPA", "SABAO",
+    "SALTO", "SENSO", "SINAL", "SITIO", "SONHO", "SOPRO", "SURDO",
+    "TARDE", "TERNO", "TERMO", "TERRA", "TIGRE", "TINTA", "TOLDO",
+    "TORRE", "TRAJE", "TREVO", "TROCO", "TRONO", "TURMA", "URUBU",
+    "VALSA", "VENTO", "VERDE", "VISAO", "VINHO", "VIUVO", "ZEBRA"
+]
+
+palavra= ['palavra1':palavras,'palavra2':palavras] 
+segredo = random.choice(palavra).upper()
+visivel = '-'* len(palavra)
+tentativas = 7
+while palavras != segredo and tentativas > 0:
+    print('------TERMO------')
+    print(visivel)
+    palpite = input("Digite a palavra:").upper()
+    if palpite != segredo:   
+        print('por favor, digite apenas letras validas')
+    novavisivel = ''
+    acertou = False
+    for pos in range(len(segredo)):
+        if palpite == segredo[pos]:
+            novavisivel+= segredo[pos]
+            acertou = True
+        else:
+            novavisivel += palavras[pos]
+    if not acertou:
+        tentativas -=1
+        print(f'letra não encontrada! tentativas restantes:{tentativas}')
+    palavraUm = novavisivel
+    palavraDois = novavisivel
+if novavisivel != segredo:
+    ultimachance = input(f'voce errou, a palavra era{segredo}')
