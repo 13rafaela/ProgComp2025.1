8fd3c9731e33b2db48daa5fc0932dd57dbaea4eb
Rafaela dos Anjos <163443768+13rafaela@users.noreply.github.com>
2025-08-07 16:01:56 -0300

    Update q2.py

--- Unidade02- Avaliacao01/q2.py
+++ Unidade02- Avaliacao01/q2.py
@@ -1,15 +1,17 @@
 #Pamela e Rafaela
-numInicial = 10
-numFinal = 100000
-contador = 0
-for numero in range(numInicial, numFinal + 1):
-    original = numero
-    inverso = 0
-    while numero > 0:
+numInicial = 10 # inicio do intervalo
+numFinal = 100000 # limite do intervalo
+contador = 0 # inicializa o contador dos num palindromos
+for numero in range(numInicial, numFinal + 1): # percorre por todos os numeros do intervalo
+    # inversão do num
+    numOriginal = numero # guarda o numero original
+    numInvertido = 0 # inicializa a variavel que vai guardar o numero invertido
+    while numero > 0: # extrai cada digito do num( do ultimo ao primeiro) e constroi o num invertido
         digito = numero % 10
-        inverso = inverso * 10 + digito
+        numInvertido = numInvertido * 10 + digito
         numero = numero // 10
-    if original == inverso:
-        contador +=1
-
+        # verificação
+    if numOriginal == numInvertido:# compara o num original com o num invertido
+        contador +=1 # se forem iguais, incrementa o contador
 print(contador)
+
